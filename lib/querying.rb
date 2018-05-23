fd5517fc3ccb9fd752250db107dd8aafff1baebd
Ji Yim <gsooyim@gmail.com>
2018-05-23 05:20:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,13 +12,13 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name, series, subgenre"
+  "SELECT authors.name, series.subgenre"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series."
+  ""
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  ""
 end
