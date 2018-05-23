40cb30b254f538ffb876425adcc3a75aa2fabb4b
Ji Yim <gsooyim@gmail.com>
2018-05-23 06:15:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN books ON series.id = books.series_id JOIN character_books ON character_books."
+  "SELECT series.title FROM series JOIN books ON series.id = books.series_id JOIN character_books ON character_books.book_id = books.id "
 end
 
 def select_character_names_and_number_of_books_they_are_in
