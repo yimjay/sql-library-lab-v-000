5e442ce02cf22164d7cdbd8c4a115b360bb65315
Ji Yim <gsooyim@gmail.com>
2018-05-23 04:54:37 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,11 +1,15 @@
 CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
-  
+  name TEXT,
+  motto TEXT,
+  species TEXT
 );
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-  
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
 );
 
 CREATE TABLE series (
