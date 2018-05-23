afb1c00ba830250dc33c1e955edfc901edc900e0
Ji Yim <gsooyim@gmail.com>
2018-05-23 04:54:00 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,26 @@
-CREATE TABLE (
+CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
   
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  sub-genre_id INTEGER
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE sub-genres(
+  id INTEGER PRIMARY KEY,
+  name TEXT
 );
